5d6a52435cd3eedff749951c0d8fa2c4387ca92d
alexander_ber <alexander.berkovich@pursway.com>
2020-07-01 15:03:20 +0300

    setup.py changed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 
 #VERSION should be defined before importing UploadCommand
-VERSION = '0.0.1b1'
+VERSION = '0.0.1b0'
 from alexber.utils import UploadCommand
 NAME = 'scrapy-feedstreaming'
 SHORT_NAME = 'spiderstate'
